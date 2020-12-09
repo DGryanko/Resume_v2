7e446d483933aa530a35a540d65e3c31aedb192d
DGryanko <fragment@gmail.com>
2020-12-10 00:43:41 +0200

    calc is done

--- js/calculator.js
+++ js/calculator.js
@@ -30,22 +30,97 @@ const technologiesMultiSelect = new Choices(technologiesSelect, {
   },
 });
 
+calculateSum();
 
 calculatorForm.addEventListener("submit", function (event) {
   event.preventDefault();
-
-// SELECTORS  
-
-  const websiteTypeSelect = document.querySelector(
-    '#calculator_form_website_type'
-  );
-  const websiteCartValue = document.querySelector('#calculator_form_input_cart input:checked');
-  const websiteReceptionValue = document.querySelector('#calculator_form_input_reception input:checked');
-// VALUES
-
-  const websiteTypeValue = websiteTypeSelect.value;
-
-  const technologiesValue = technologiesMultiSelect.getValue()
-  console.log(websiteCartValue.value, websiteReceptionValue.value);
+  calculateSum();
 });
 
+function calculateSum() {
+  // SELECTORS
+
+  const websiteTypeSelect = document.querySelector(
+    "#calculator_form_website_type"
+  );
+  const websiteCartSelect = document.querySelector(
+    "#calculator_form_input_cart input:checked"
+  );
+  const websiteReceptionSelect = document.querySelector(
+    "#calculator_form_input_reception input:checked"
+  );
+  // VALUES
+
+  const websiteTypeValue = extractPriceFromValue(websiteTypeSelect.value);
+  const websiteCartValue = convertCartOptionToPrice(websiteCartSelect.value);
+  const websiteReceptionValue = convertReceptionOptionToPrice(
+    websiteReceptionSelect.value
+  );
+  const technologiesValue = getTechnologiesSum(
+    technologiesMultiSelect.getValue()
+  );
+
+  const totalSum =
+    websiteTypeValue +
+    technologiesValue +
+    websiteCartValue +
+    websiteReceptionValue;
+
+  renderSum(totalSum);
+}
+
+function renderSum(sum) {
+  const costElement = document.querySelector("#calculator_form_total_cost");
+
+  costElement.textContent = "Calculating";
+
+  setTimeout(function () {
+    costElement.textContent = "Calculating.";
+  }, 250);
+
+  setTimeout(function () {
+    costElement.textContent = "Calculating..";
+  }, 500);
+
+  setTimeout(function () {
+    costElement.textContent = "Calculating...";
+  }, 750);
+
+  setTimeout(function () {
+    costElement.textContent = sum + "$";
+  }, 1000);
+}
+
+function convertCartOptionToPrice(option) {
+  if (option === "Yes") {
+    return 300;
+  }
+
+  return 0;
+}
+
+function convertReceptionOptionToPrice(option) {
+  if (option === "Yes") {
+    return 500;
+  }
+
+  return 0;
+}
+
+function getTechnologiesSum(technologiesArr) {
+  let totalSum = 0;
+  technologiesArr.forEach(function (tech) {
+    totalSum = totalSum + extractPriceFromValue(tech.value);
+  });
+  return totalSum;
+}
+
+function extractPriceFromValue(str) {
+  const price = str.match(/:\d+/);
+
+  if (price) {
+    return Number(price[0].slice(1)) || 0;
+  }
+
+  return 0;
+}
